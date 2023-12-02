4dbfad378f4457844726846d956010e7ee0edbc9
henry <henry@henrys-MacBook-Air.local>
2023-12-02 15:14:26 -0800

    better mountains, fixed some class issues

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import {defs, tiny} from './examples/common.js';
 import {tetris} from './tetris.js';
 
 const {
-    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Shape_From_File, Material, Scene, Texture, Textured_Phong, Fake_Bump_Map,
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Matrix, Mat4, Light, Shape, Material, Scene, Texture, Textured_Phong, Fake_Bump_Map,
 } = tiny;
 
 
@@ -83,6 +83,8 @@ class Base_Scene extends Scene {
             big_cone: new defs.Closed_Cone(20, 20),
 
             //mountain_obj: new Shape_From_File("assets/mountainpeak.obj")
+            mountain_obj: new Shape_From_File("assets/part.obj")
+
         };
         this.materials = {
             brown_texture: new Material(new defs.Phong_Shader(),
@@ -381,10 +383,8 @@ export class Main extends Base_Scene {
     }
 
     drawObjMountain(context, program_state, matrix){
-        let mountain_rotation = Mat4.translation(0, 0.5, 0).times(Mat4.rotation(-Math.PI / 2, 1, 0, 0));
-        let mountain_postScale = Mat4.scale(40, 28, 40).times(mountain_rotation);
-        mountain_postScale = Mat4.scale(40, 28, 40);
-        let mountain_final = matrix.times(mountain_postScale);
+        let mountain_postScale = Mat4.scale(40, 70, 40);
+        let mountain_final = matrix.times(Mat4.rotation(Math.PI / 2, 0, 1, 0).times(mountain_postScale));
         this.shapes.mountain_obj.draw(context, program_state, this.downscale_mat4.times(mountain_final), this.materials.mountain);
 
         /*let top_postScale = Mat4.translation(0, 38-4, 0).times(Mat4.scale(8, 5.6, 8).times(mountain_rotation));
@@ -471,7 +471,7 @@ export class Main extends Base_Scene {
         }
 
         for(var i = 0; i < 3; i++){
-            this.drawMountain(context, program_state, this.mountainMatrices[i]);
+            this.drawObjMountain(context, program_state, this.mountainMatrices[i]);
         }
 
         this.drawGround(context, program_state, Mat4.translation(0, -3, 0));
@@ -501,5 +501,107 @@ export class Main extends Base_Scene {
 
 
 
+    }
+}
+
+
+export class Shape_From_File extends Shape {                                   // **Shape_From_File** is a versatile standalone Shape that imports
+    // all its arrays' data from an .obj 3D model file.
+    constructor(filename) {
+        super("position", "normal", "texture_coord");
+        // Begin downloading the mesh. Once that completes, return
+        // control to our parse_into_mesh function.
+        this.load_file(filename);
+    }
+
+    load_file(filename) {                             // Request the external file and wait for it to load.
+        // Failure mode:  Loads an empty shape.
+        return fetch(filename)
+            .then(response => {
+                if (response.ok) return Promise.resolve(response.text())
+                else return Promise.reject(response.status)
+            })
+            .then(obj_file_contents => this.parse_into_mesh(obj_file_contents))
+            .catch(error => {
+                this.copy_onto_graphics_card(this.gl);
+            })
+    }
+
+    parse_into_mesh(data) {                           // Adapted from the "webgl-obj-loader.js" library found online:
+        var verts = [], vertNormals = [], textures = [], unpacked = {};
+
+        unpacked.verts = [];
+        unpacked.norms = [];
+        unpacked.textures = [];
+        unpacked.hashindices = {};
+        unpacked.indices = [];
+        unpacked.index = 0;
+
+        var lines = data.split('\n');
+
+        var VERTEX_RE = /^v\s/;
+        var NORMAL_RE = /^vn\s/;
+        var TEXTURE_RE = /^vt\s/;
+        var FACE_RE = /^f\s/;
+        var WHITESPACE_RE = /\s+/;
+
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i].trim();
+            var elements = line.split(WHITESPACE_RE);
+            elements.shift();
+
+            if (VERTEX_RE.test(line)) verts.push.apply(verts, elements);
+            else if (NORMAL_RE.test(line)) vertNormals.push.apply(vertNormals, elements);
+            else if (TEXTURE_RE.test(line)) textures.push.apply(textures, elements);
+            else if (FACE_RE.test(line)) {
+                var quad = false;
+                for (var j = 0, eleLen = elements.length; j < eleLen; j++) {
+                    if (j === 3 && !quad) {
+                        j = 2;
+                        quad = true;
+                    }
+                    if (elements[j] in unpacked.hashindices)
+                        unpacked.indices.push(unpacked.hashindices[elements[j]]);
+                    else {
+                        var vertex = elements[j].split('/');
+
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 0]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 1]);
+                        unpacked.verts.push(+verts[(vertex[0] - 1) * 3 + 2]);
+
+                        if (textures.length) {
+                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 0]);
+                            unpacked.textures.push(+textures[((vertex[1] - 1) || vertex[0]) * 2 + 1]);
+                        }
+
+                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 0]);
+                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 1]);
+                        unpacked.norms.push(+vertNormals[((vertex[2] - 1) || vertex[0]) * 3 + 2]);
+
+                        unpacked.hashindices[elements[j]] = unpacked.index;
+                        unpacked.indices.push(unpacked.index);
+                        unpacked.index += 1;
+                    }
+                    if (j === 3 && quad) unpacked.indices.push(unpacked.hashindices[elements[0]]);
+                }
+            }
+        }
+        {
+            const { verts, norms, textures } = unpacked;
+            for (var j = 0; j < verts.length / 3; j++) {
+                this.arrays.position.push(vec3(verts[3 * j], verts[3 * j + 1], verts[3 * j + 2]));
+                this.arrays.normal.push(vec3(norms[3 * j], norms[3 * j + 1], norms[3 * j + 2]));
+                this.arrays.texture_coord.push(vec(textures[2 * j], textures[2 * j + 1]));
+            }
+            this.indices = unpacked.indices;
+        }
+        this.normalize_positions(false);
+        this.ready = true;
+    }
+
+    draw(context, program_state, model_transform, material) {               // draw(): Same as always for shapes, but cancel all
+        // attempts to draw the shape before it loads:
+        if (this.ready)
+            super.draw(context, program_state, model_transform, material);
     }
 }
